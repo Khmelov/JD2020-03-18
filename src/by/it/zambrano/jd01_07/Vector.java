bf2fcbec9632746d65c055589dbd180463ea373d
Luz <luzzambrano277@gmail.com>
2020-04-12 14:24:57 +0300

    jd01_08B

--- src/by/it/zambrano/jd01_07/Vector.java
+++ src/by/it/zambrano/jd01_07/Vector.java
@@ -1,25 +1,41 @@
 package by.it.zambrano.jd01_07;
 
 
-    class Vector extends Var {
+import java.util.Arrays;
+
+class Vector extends Var {
         private double[] value;
 
         Vector(double[] value) {
             this.value = value;
         }
 
-
-
-              @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder("{");
-            String delimiter="";
-            for (double element:value) {
-                sb.append(delimiter).append(element);
-                delimiter = ", ";
+        public Vector(String strVector) {
+            StringBuilder sb = new StringBuilder(strVector);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(0);
+            String[] strArray = sb.toString().split(",");
+            double[] doubleArray = new double[strArray.length];
+            for (int i = 0; i < doubleArray.length; i++) {
+                doubleArray[i] = Double.parseDouble(strArray[i]);
             }
-                    sb.append("}");
-                    return sb.toString();
+            this.value = doubleArray;
         }
-    }
+
+        Vector(Vector vector) {
+            this.value = Arrays.copyOf(vector.value, vector.value.length);
+        }
+            @Override
+            public String toString() {
+                StringBuilder sb = new StringBuilder("{");
+                String delimiter = "";
+                for (double element : value) {
+                    sb.append(delimiter).append(element);
+                    delimiter = ", ";
+                }
+                sb.append("}");
+                return sb.toString();
+            }
+        }
+
 
