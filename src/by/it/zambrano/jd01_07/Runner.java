79d1a173911a7da7b299b13efd7093a84880f6ba
Luz <luzzambrano277@gmail.com>
2020-04-12 22:29:43 +0300

    jd01_10B1

--- src/by/it/zambrano/jd01_07/Runner.java
+++ src/by/it/zambrano/jd01_07/Runner.java
@@ -4,11 +4,7 @@ class Runner {
 
     public static void main(String[] args) {
         Var a1 = new Scalar(3.14);
-<<<<<<< HEAD
         Var a2 = new Vector(new double[]{1, 2, 3});
-=======
-        Var a2 =new Vector(new double[]{1,2,3});
->>>>>>> jd01_06B1
         System.out.println(a1);
         System.out.println(a2);
 
