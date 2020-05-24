20af4613b671f4efd025db283739fb6a1a138376
novikov1 <mnovikov1310@gmail.com>
2020-05-24 20:52:57 +0300

    JD02_04 A

--- src/by/it/novikov/calc/Scalar.java
+++ src/by/it/novikov/calc/Scalar.java
@@ -1,64 +1,71 @@
 package by.it.novikov.calc;
 
-class Scalar extends Var implements Operation {
-    private double value;
+class Scalar extends Var {
 
-    public double getValue() {
-        return value;
-    }
+    private double value;
 
     Scalar(double value) {
         this.value = value;
     }
 
-     Scalar(Scalar scalar) {
-        this.value = scalar.value;
+    Scalar(Scalar other) {
+        this.value = other.value;
     }
 
-     Scalar(String strScalar) {
-        this.value = Double.parseDouble(strScalar);
+    Scalar(String strValue) {
+        this.value = Double.parseDouble(strValue);
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public Var add(Var other) throws CalcException {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = value + otherValue;
+            return new Scalar(result);
+        }
+        return other.add(this);
+    }
+
+    @Override
+    public Var sub(Var other) throws CalcException {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = value - otherValue;
+            return new Scalar(result);
+        }
+        return other.sub(this).mul(new Scalar(-1));
+    }
+
+    @Override
+    public Var mul(Var other) throws CalcException {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = value * otherValue;
+            return new Scalar(result);
+        }
+        return other.mul(this);
+    }
+
+    @Override
+    public Var div(Var other) throws CalcException {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            if (otherValue != 0) {
+                double result = value / otherValue;
+                return new Scalar(result);
+            }
+            else
+                throw new CalcException("divide by zero");
+        }
+        return super.div(other);
     }
 
     @Override
     public String toString() {
         return Double.toString(value);
-    }
-
-    @Override
-    public Var add(Var other) throws CalcException{
-         if (other instanceof Scalar){
-             double result = this.value + ((Scalar) other).value;
-             return new Scalar(result);
-         }
-        return other.add(this);
-    }
-
-    @Override
-    public Var sub(Var other) throws CalcException{
-         if (other instanceof Scalar){
-             double result = this.value - ((Scalar) other).value;
-             return new Scalar(result);
-         }
-        return other.sub(this).mul(new Scalar(-1));
-    }
-
-    @Override
-    public Var mul(Var other) throws CalcException{
-         if (other instanceof Scalar){
-             double result = this.value * ((Scalar) other).value;
-             return new Scalar(result);
-         }
-        return other.mul(this);
-    }
-
-    @Override
-    public Var div(Var other) throws CalcException{
-        if (other instanceof Scalar){
-            if(((Scalar) other).value==0)
-                throw new CalcException("Деление на ноль");
-            double result = this.value / ((Scalar) other).value;
-            return new Scalar(result);
-        }
-        return super.div(other);
     }
 }
--- src/by/it/novikov/calc/Var.java
+++ src/by/it/novikov/calc/Var.java
@@ -1,67 +1,50 @@
 package by.it.novikov.calc;
 
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 abstract class Var implements Operation {
 
-    private static Map<String, Var> vars = new HashMap<>();
-
-    static Var saveVar(String name, Var var) {
-        vars.put(name, var);
-return var;
-    }
-    static void printvar (){
-        List<String> keys = new ArrayList<>(vars.keySet());
-        for (String key : keys) {
-            System.out.println(key + "=" + vars.get(key));
-        }
-    }
-
-
-    @Override
-    public String toString() {
-        return "abstract Var";
-    }
+    private static Map<String,Var> map=new HashMap<>();
 
     @Override
     public Var add(Var other) throws CalcException {
-        throw new CalcException("Операция " + this + " + " + other + " невозможна");
+        throw new CalcException(String.format("Operation %s + %s is impossible\n",this,other));
     }
 
     @Override
-    public Var sub(Var other) throws CalcException{
-        throw new CalcException("Операция " + this + " - " + other + " невозможна");
+    public Var sub(Var other) throws CalcException {
+        throw new CalcException(String.format("Operation %s - %s is impossible\n",this,other));
     }
 
     @Override
-    public Var mul(Var other) throws CalcException{
-        throw new CalcException("Операция " + this + " * " + other + " невозможна");
+    public Var mul(Var other) throws CalcException {
+        throw new CalcException(String.format("Operation %s * %s is impossible\n",this,other));
     }
 
     @Override
-    public Var div(Var other) throws CalcException{
-        throw new CalcException("Операция " + this + " / " + other + " невозможна");
+    public Var div(Var other) throws CalcException {
+        throw new CalcException(String.format("Operation %s / %s is impossible\n",this,other));
     }
 
-    static Var create(String strVar) throws CalcException {
-        strVar = strVar.trim().replace(" ", "");
-        if (strVar.matches(Patterns.SCALAR)) {
+    @Override
+    public String toString() {
+        return "Abstract Var";
+    }
+
+    static Var createVar(String strVar) throws CalcException {
+        if (strVar.matches(Patterns.SCALAR))
             return new Scalar(strVar);
-        } else if (strVar.matches(Patterns.VECTOR)) {
-            return new Vector(strVar);
-        } else if (strVar.matches(Patterns.MATRIX)) {
-            return new Matrix((strVar));
-        } else if (vars.containsKey(strVar)) {
-            return vars.get(strVar);
-
+        else {
+            Var var = map.get(strVar);
+            if (var!=null)
+                return var;
+            else
+                throw new CalcException("incorrect name var: "+strVar);
         }
-        throw new CalcException("Невозможно создать " + strVar);
+    }
+
+    public static void save(String name, Var var) {
+        map.put(name,var);
     }
 }
-
-
-
